c92a0c07b9330c5f892fe9ba180eab070285762b
emily <eli.mhs24@gmail.com>
2023-07-20 11:50:54 -0500

    Initial Commit

---
+++ calendarApp/calendarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  calendarAppApp.swift
+//  calendarApp
+//
+//  Created by Scholar on 7/20/23.
+//
+
+import SwiftUI
+
+@main
+struct calendarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
